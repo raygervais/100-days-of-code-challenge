ad4b4b213538b875ef040db715bb03d5c64cbd09
RayGervais <raygervais@hotmail.ca>
2018-05-28 22:20:51 -0400

    new(Python): Initalize Django using python3-venv for Django-Vue

---
+++ week-5/django-vue/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/raygervais/Developer/Repositories/100-days-of-code-challenge/week-5/django-vue/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
