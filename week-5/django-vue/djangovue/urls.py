2118ce26d3a3f6b50c1408fd663b7b3fa22cdea8
RayGervais <raygervais@hotmail.ca>
2018-05-29 23:28:54 -0400

    new(Python): Day 26 Completed! Private vs Public Client EndPoints in Django using Auth0

--- week-5/django-vue/djangovue/urls.py
+++ week-5/django-vue/djangovue/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from catalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/public/', views.public),
+    url(r'^api/private/', views.private)
 ]
