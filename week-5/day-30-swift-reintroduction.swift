f39b77cc8e017d20506ee9370a672e2261ada61c
RayGervais <raygervais@hotmail.ca>
2018-06-21 00:21:25 -0400

    update(Swift): Fixes and extends day 1 tutorial

--- week-5/day-30-swift-reintroduction.swift
+++ week-5/day-30-swift-reintroduction.swift
@@ -15,10 +15,10 @@ var osVersionList = [
     "watchOS": "4"
 ]
 
-var leakList =[String]()
+var leakList = [String]()
 
 // Control Flow
-let days = 0..100
+let days = 0...100
 for day in days {
     if day % 2 == 0 {
         print("Even")
@@ -32,5 +32,44 @@ let nickName: String? = nil
 let defaultName = "User"
 let greeting = "Good Morning \(nickName ?? defaultName)!"
 
+// Functions and Function Values
+func makeIncrementor() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
 
+var increment = makeIncrementor()
+var inc = increment(10)
 
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
+
+// Class
+
+class Shape {
+    var numberOfSides = 0
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+
+var shape = Shape()
+shape.numberOfSides = 11
+var shapeDesc = shape.simpleDescription()
